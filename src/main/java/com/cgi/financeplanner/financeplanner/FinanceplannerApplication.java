0988f9d95b95b913f03327488fdbd5c60ed691f1
Jannik <jannik_meier@yahoo.de>
2018-03-12 11:27:30 +0100

    setting up spring-boot gradle files

---
+++ src/main/java/com/cgi/financeplanner/financeplanner/FinanceplannerApplication.java
@@ -0,0 +1,12 @@
+package com.cgi.financeplanner.financeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceplannerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cgi/financeplanner/financeplanner/FinanceplannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cgi.financeplanner.financeplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinanceplannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
