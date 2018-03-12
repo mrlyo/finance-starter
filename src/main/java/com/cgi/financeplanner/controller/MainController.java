6839a1be158372a750c1c2ef17d3b0eb9ace141d
mrlyo <jannik_meier@yahoo.de>
2018-03-12 14:20:20 +0100

    first working state

---
+++ src/main/java/com/cgi/financeplanner/controller/MainController.java
@@ -0,0 +1,4 @@
+package com.cgi.financeplanner.controller;
+
+public class MainController {
+}
---
+++ src/test/java/com/cgi/financeplanner/financeplanner/MainControllerTest.java
@@ -0,0 +1,4 @@
+package com.cgi.financeplanner.financeplanner;
+
+public class MainControllerTest {
+}
